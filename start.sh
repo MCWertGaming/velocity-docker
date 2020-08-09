2e8a1e4871169800074868d78e96103c1e255f7a
Damon Leven <mcwertgaming@gmail.com>
2020-08-09 18:54:32 +0200

    added luckyperms

--- start.sh
+++ start.sh
@@ -13,6 +13,13 @@ VELOCITY_RAW="$(curl -s https://ci.velocitypowered.com/job/velocity-1.1.0/lastBu
 VELOCITY_FILE="${VELOCITY_RAW:41:-1}"
 VELOCITY_URL="https://ci.velocitypowered.com/job/velocity-1.1.0/lastBuild/artifact/proxy/build/libs/$VELOCITY_FILE"
 wget $VELOCITY_URL -O $BUILD_DIR/velocity.jar
+# download plugins
+LUCKYP_RAW="$(curl -s https://ci.lucko.me/job/LuckPerms/lastSuccessfulBuild//api/python?pretty=true | grep velocity)"
+LUCKYP_FILE="${LUCKYP_RAW:44:-1}"
+LUCKYP_URL="https://ci.lucko.me/job/LuckPerms/1145/artifact/velocity/build/libs/$LUCKYP_FILE"
+wget $LUCKYP_URL -O $BUILD_DIR/plugins/
+
+
 # build & start container
 podman build -t velocity .
 podman run $DOCKER_FLAGS velocity
